343694d1ebcac942326c8ab291479700e10bf737
tianyugu <tianyugu@tianyugus-Air.wv.cc.cmu.edu>
2017-04-08 16:46:00 -0400

    added a temptest

---
+++ 214hw5Framework/src/test/main/java/framework/core/temptest.java
@@ -0,0 +1,13 @@
+package test.main.java.framework.core;
+
+import org.junit.Test;
+/**
+ * Created by tianyugu on 4/8/17.
+ */
+public class temptest {
+
+    @Test
+    public void test() {
+        
+    }
+}
