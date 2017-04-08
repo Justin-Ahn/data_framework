f8615eae334cd5d394677d5577778472f545c34d
Justin Ahn <Justin Ahn>
2017-04-08 15:00:24 -0400

    Eclipse Compatibility

--- framework/src/main/java/Team35/DataFramework/gui/DataFrameworkGui.java
+++ framework/src/main/java/Team35/DataFramework/gui/DataFrameworkGui.java
@@ -1,7 +1,13 @@
 package Team35.DataFramework.gui;
 
 /**
- * Created by Justin on 4/8/2017.
+ * Team 35's Data Visualization Framework GUI Implementation.
+ * The GUI is responsible for the user to change between multiple Data Plugins and multiple Data Visualization plugins.
+ * @link TODO
+ * A lot of the implementation of this Data Framework GUI was inspired by 15-214 Recitation 8.
  */
 public class DataFrameworkGui {
+
+
 }
+
