8eb0ea84056f75f4d3abe2e632fc7ffbcce453ef
tianyugu <tianyugu@tianyugus-Air.wv.cc.cmu.edu>
2017-04-08 18:01:27 -0400

    foo added

---
+++ framework/src/main/java/core/foo.java
@@ -0,0 +1,10 @@
+package main.java.core;
+
+/**
+ * Created by tianyugu on 4/8/17.
+ */
+public class foo {
+    public static void main(String[] args) {
+        System.out.println("kkk");
+    }
+}
--- framework/src/main/java/core/temp.java
+++ framework/src/main/java/core/temp.java
@@ -4,4 +4,7 @@ package main.java.core;
  * Created by Justin on 4/8/2017.
  */
 public class temp {
+    public static void main(String[] args) {
+        System.out.println("!!!");
+    }
 }
