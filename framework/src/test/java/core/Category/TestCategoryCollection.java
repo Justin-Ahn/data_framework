09ebc6439b0a217626a26bad8355950adeab0845
tianyugu <tianyugu@tianyugus-MacBook-Air.local>
2017-04-08 22:34:25 -0400

    save for pull

---
+++ framework/src/test/java/core/Category/TestCategoryCollection.java
@@ -0,0 +1,19 @@
+package core.Category;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import core.Category.SpecificCategory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by tianyugu on 4/8/17.
+ */
+public class TestCategoryCollection {
+
+
+}
--- framework/src/test/java/core/Category/TestSpecificCategory.java
+++ framework/src/test/java/core/Category/TestSpecificCategory.java
@@ -19,6 +19,10 @@ public class TestSpecificCategory {
     public void testSpecificCategory1(){
         SpecificCategory c = new SpecificCategory("t1","n1");
         assertEquals("t1",c.getType());
+        assertEquals("n1",c.getName());
+
+        SpecificCategory d = new SpecificCategory("t1","n1");
+        assertTrue(c.equals(d));
 
     }
 }
